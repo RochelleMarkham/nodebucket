7c71f39f62b70928be27a96a3460f60101951961
Rochelle Markham <rmarkham@my365.bellevue.edu>
2020-12-11 19:41:43 -0600

    Code attribution

--- server/db-models/item.js
+++ server/db-models/item.js
@@ -1,3 +1,11 @@
+/*======================================
+; Title: item.js
+; Author: Professor Krasso
+; Date: 12/9/20
+; Modified By: Rochelle Markham
+; Description: nodebucket-app
+======================================*/
+
 const mongoose = require('mongoose');
 
 let itemSchema = mongoose.Schema({
